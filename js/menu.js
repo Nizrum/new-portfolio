b4ebd6aa5e67b699ecd36940d42534433971f3c4
nizrum <sema2005@icloud.com>
2023-03-19 15:07:23 +0400

    Доработан функционал меню

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,30 @@
-const menuBtn = document.querySelector('.menu__button');
-const menuIcon = document.querySelector('.menu__icon');
-const menuList = document.querySelector('.menu__list');
+const menuBtn = document.querySelector(".menu__button");
+const menuIcon = document.querySelector(".menu__icon");
+const menuList = document.querySelector(".menu__list");
+const menuLinks = document.querySelectorAll(".menu__link");
 
-menuBtn.addEventListener('click', () => {
-    menuList.classList.toggle('menu__list_active');
-    menuIcon.classList.toggle('open');
+const closeMenu = () => {
+    menuList.classList.remove("menu__list_active");
+    menuIcon.classList.remove("open");
+};
+
+menuBtn.addEventListener("click", () => {
+    menuList.classList.toggle("menu__list_active");
+    menuIcon.classList.toggle("open");
+});
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", closeMenu);
+});
+
+document.body.addEventListener("click", (event) => {
+    if (!event.target.closest(".menu__list") && !event.target.closest(".menu__button")) {
+        closeMenu();
+    }
+});
+
+document.body.addEventListener("keydown", (event) => {
+    if (event.key == "Escape") {
+        closeMenu();
+    }
 });
